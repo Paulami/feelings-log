f0e8cd5dc5fdcf5c782730fd2e292257f0c67087
Paulami <3.paulami@gmail.com>
2020-03-22 03:19:54 -0400

    Connected to mongoDb Atlas

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const app = express();
 const config = require('./config');
+// read in mongoose library
+const mongoose = require('mongoose');
+
+const app = express();
+
+// ---- Connect to mongodb here ----
+// read in the URI to our MongoDB Atlas 
+const MONGODB_URI = config.MONGODB_URI;
+// Use mongoose to connect to our MongoDB Atlas server
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
+.then(() => {
+  console.log("Connected to MongoDB!!");
+})
+.catch((err) => {
+  console.log(err);
+})
+
+// --- connect to your collection ---
 
 const PORT = config.PORT;
 
